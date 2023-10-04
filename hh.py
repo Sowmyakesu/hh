2e78e2db076e7c06c4199cbd1a05eb4092ccafb2
sowmya <kommabathulasowmya1996@gmail.com>
2023-10-04 12:47:03 -0500

    Merge remote changes into main

--- hh.py
+++ hh.py
@@ -5,6 +5,7 @@
 
 
 # Import necessary libraries
+import os
 import pandas as pd
 from flask import Flask, render_template, request
 from sklearn.linear_model import LogisticRegression
@@ -13,8 +14,12 @@ import numpy as np
 # Create a Flask web application
 app = Flask(__name__, template_folder='templates')
 
-# Load your dataset
-heart_df = pd.read_csv(r"C:\Users\komma\Desktop\heart\framingham.csv")
+# Get the current directory of the script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Load your dataset with a relative file path
+dataset_path = os.path.join(current_dir, 'framingham.csv')
+heart_df = pd.read_csv(dataset_path)
 heart_df.drop(['education'], axis=1, inplace=True)
 heart_df.dropna(axis=0, inplace=True)
 
@@ -25,39 +30,41 @@ y = new_features.iloc[:, -1]
 logreg = LogisticRegression(solver='liblinear')
 logreg.fit(x, y)
 
-# Define a route to serve an HTML form for user input
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        # Get user input from the form
-        age = float(request.form['age'])
-        sex = float(request.form['sex'])
-        cigsPerDay = float(request.form['cigsPerDay'])
-        totChol = float(request.form['totChol'])
-        sysBP = float(request.form['sysBP'])
-        glucose = float(request.form['glucose'])
-
-        # Create a NumPy array from user inputs
-        user_data = np.array([[age, sex, cigsPerDay, totChol, sysBP, glucose]])
-
-        # Use the trained model to predict heart disease risk
-        heart_disease_prob = logreg.predict_proba(user_data)[:, 1]
-
-        # Determine risk level and provide suggestions
-        if heart_disease_prob < 0.2:
-            risk_level = "Low"
-            suggestions = ["Maintain a healthy lifestyle.", "Regularly monitor your health."]
-        elif 0.2 <= heart_disease_prob < 0.6:
-            risk_level = "Moderate"
-            suggestions = ["Consult with a healthcare professional for personalized advice.",
-                           "Consider making lifestyle changes like diet and exercise."]
-        else:
-            risk_level = "High"
-            suggestions = ["Urgently consult with a healthcare professional.", "Follow medical advice and treatment."]
-
-        return render_template('result.html', risk_level=risk_level, heart_disease_prob=heart_disease_prob[0], suggestions=suggestions)
-
+# Define a route to serve an HTML form for user input using GET
+@app.route('/', methods=['GET'])
+def get_index():
     return render_template('index.html')
+
+# Define a route to handle form submission using POST
+@app.route('/', methods=['POST'])
+def post_index():
+    # Get user input from the form (similar to your existing POST route)
+    age = float(request.form['age'])
+    sex = float(request.form['sex'])
+    cigsPerDay = float(request.form['cigsPerDay'])
+    totChol = float(request.form['totChol'])
+    sysBP = float(request.form['sysBP'])
+    glucose = float(request.form['glucose'])
+
+    # Create a NumPy array from user inputs
+    user_data = np.array([[age, sex, cigsPerDay, totChol, sysBP, glucose]])
+
+    # Use the trained model to predict heart disease risk
+    heart_disease_prob = logreg.predict_proba(user_data)[:, 1]
+
+    # Determine risk level and provide suggestions
+    if heart_disease_prob < 0.2:
+        risk_level = "Low"
+        suggestions = ["Maintain a healthy lifestyle.", "Regularly monitor your health."]
+    elif 0.2 <= heart_disease_prob < 0.6:
+        risk_level = "Moderate"
+        suggestions = ["Consult with a healthcare professional for personalized advice.",
+                       "Consider making lifestyle changes like diet and exercise."]
+    else:
+        risk_level = "High"
+        suggestions = ["Urgently consult with a healthcare professional.", "Follow medical advice and treatment."]
+
+    return render_template('result.html', risk_level=risk_level, heart_disease_prob=heart_disease_prob[0], suggestions=suggestions)
 
 if __name__ == '__main__':
     app.run(debug=True)
